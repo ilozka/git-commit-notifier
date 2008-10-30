bea3ee98eb60d9bc4e1a198458e8466d7e6b83e2
zoltan <ilozkaa@yahoo.com>
2008-10-30 13:47:10 +0200

    remove unneded config.rb file

--- git_commit_notifier/config/config.rb
+++
@@ -1,2 +0,0 @@
-RECIPIENT_ADDRESS = nil # if given, takes precedence over git config hooks.mailinglist value
-APPLICATION_NAME = nil  # if given, takes precedence over git config hooks.emailprefix value
