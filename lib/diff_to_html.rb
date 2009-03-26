2b6b084c997eca72662c530895e14ce1412d9a4e
Brian Donovan <brian.donovan@gmail.com>
2009-03-26 09:43:32 -0700

    Fixes that current_list is nil when calling Array#[] with indicies that are out of range.

--- lib/diff_to_html.rb
+++ lib/diff_to_html.rb
@@ -256,7 +256,7 @@ class DiffToHtml
     previous_file = THIS_FILE ? File.join(File.dirname(THIS_FILE), "../config/previously.txt") : "/tmp/previously.txt"
     previous_list = (File.read(previous_file).split("\n") if File.exist?(previous_file)) || []
     commits.reject!{|c| c.find{|sha| previous_list.include?(sha)} }
-    current_list = (previous_list + commits.flatten)[-1000..-1]
+    current_list = (previous_list + commits.flatten).last(1000)
     File.open(previous_file, "w"){|f| f << current_list.join("\n") } unless current_list.empty?
 
     commits.each_with_index do |commit, i|
